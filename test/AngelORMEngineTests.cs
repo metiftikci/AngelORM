5c974a42690ba7817793ef393e36542458bec6b2
Muhammed TİFTİKÇİ <jaqra@hotmail.com>
2019-06-30 11:34:44 +0300

    Added engine tests

---
+++ test/AngelORMEngineTests.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AngelORM.Tests.Models;
+using Xunit;
+
+namespace AngelORM.Tests
+{
+    public class AngelORMEngineTests
+    {
+        private const string CS = "Server=MONSTER\\MET; Database=AngelORM; Integrated Security=True;";
+
+        private AngelORMEngine _engine = new AngelORMEngine(CS);
+
+        [Fact]
+        public void ToListTest()
+        {
+            List<User> users = _engine.ToList<User>();
+
+            Assert.Equal("muhammed", users[0].Username);
+            Assert.Equal("jaqra@hotmail.com", users[0].Email);
+        }
+
+        [Fact]
+        public void InsertTest()
+        {
+            User user = new User();
+            user.Name = "foo";
+            user.Username = "bar";
+            user.Password = "qwerty";
+            user.Password = "baz@qux.com";
+
+            _engine.Insert(user);
+        }
+
+        [Fact]
+        public void InsertTest_check_inserted_key()
+        {
+            int lastUserId = (int)(decimal)_engine.ExecuteScalar("SELECT IDENT_CURRENT('User')");
+
+            User user = new User();
+            user.Name = "foo";
+            user.Username = "bar";
+            user.Password = "qwerty";
+            user.Password = "baz@qux.com";
+
+            _engine.Insert(user);
+
+            Assert.Equal(lastUserId + 1, user.Id);
+        }
+
+        [Fact]
+        public void UpdateTest()
+        {
+            List<User> users = _engine.ToList<User>();
+            User lastUser = users.Last();
+
+            lastUser.Username = Guid.NewGuid().ToString();
+
+            _engine.Update(lastUser);
+            
+            List<User> users2 = _engine.ToList<User>();
+            User lastUser2 = users2.Last();
+
+            Assert.Equal(lastUser.Username, lastUser2.Username);
+        }
+
+        [Fact]
+        public void DeleteTest()
+        {
+            List<User> users = _engine.ToList<User>();
+            User lastUser = users.Last();
+
+            _engine.Delete(lastUser);
+            
+            List<User> users2 = _engine.ToList<User>();
+            User lastUser2 = users2.Last();
+
+            Assert.NotEqual(lastUser.Id, lastUser2.Id);
+        }
+    }
+}
